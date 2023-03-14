82a001a4685656856c8c33e52eb8d37945eebcd9
Dmytro2V <op2vin@gmail.com>
2023-03-15 00:54:44 +0200

    done all: LongPractice2, LongPractice3, phases 12,3,4,5,6,7 from both practices, bonus phase1, phase2, bonus phase3 /main tasks, skipping minor/

--- server/routes/joined.js
+++ server/routes/joined.js
@@ -63,25 +63,16 @@ router.get('/insects-trees', async (req, res, next) => {
         // let get trees:
         const insectTrees = await insect.getTrees({
             attributes: ['id', 'tree'],
-            order: [['tree', 'asc']]
-        });        
-       // console.log(insectTrees);
-        const insectTreesForOutput = insectTrees.map(itree => {
-            return {
-                    id:itree.id,
-                    tree: itree.tree        
-                    }
-            }
-        );
-        console.log('insectTreesForOutput', insectTreesForOutput);
-
-
-        payload.push({
+            order: [['tree', 'asc']],
+            joinTableAttributes: [] // remove extra data
+        });             
+       
+         payload.push({ // add if (insectTrees.length > 0) for hide insects without trees
             id: insect.id,
             name: insect.name,
             description: insect.description,
-            trees: insectTreesForOutput 
-        });
+            trees: insectTrees //insectTreesForOutput 
+            });
     }
 
     res.json(payload);
@@ -115,6 +106,111 @@ router.get('/insects-trees', async (req, res, next) => {
  *   - (Any others you think of)
  */
 // Your code here
+
+router.post('/associate-tree-insect', async (req, res, next) => {
+    // The request body is expected to have two attributes: tree and insect
+    try {
+        console.log('req.body' , req.body);
+        let tree = {};
+        let insect = {};
+
+        if (!req.body.tree) {
+            next({
+                status: "error",
+                message: `Could not update tree`,
+                details: 'tree object not exist in request'
+            });
+        } else if (req.body.tree.id) {
+            tree = await Tree.findByPk(req.body.tree.id)
+            if (!tree) {
+                next({
+                    status: "error",
+                    message: `Could not update tree`,
+                    details: `Tree not exist with id ${req.body.tree.id}`
+                }); 
+            }; 
+        } else { // tree exist with body, saving
+                // here we can also check all keys exists
+                
+                tree = await Tree.create({
+                    tree: req.body.tree.name,
+                    location: req.body.tree.location,
+                    heightFt: req.body.tree.height,
+                    groundCircumferenceFt: req.body.tree.size
+                });
+            
+               
+        };
+        
+        // same for insect
+        if (!req.body.insect) {
+            next({
+                status: "error",
+                message: `Could not update insect`,
+                details: 'insect object not exist in request'
+            });
+        } else if (req.body.insect.id) {
+            insect = await Insect.findByPk(req.body.insect.id)
+            if (!insect) {
+                next({
+                    status: "error",
+                    message: `Could not update insect`,
+                    details: `Insect not exist with id ${req.body.insect.id}`
+                }); 
+            } 
+        } else { // tree exist with body, saving
+                // here we can also check all keys exists
+                insect = await Insect.create({
+                    name: req.body.insect.name,
+                    description: req.body.insect.description,
+                    fact: req.body.insect.fact,
+                    territory: req.body.insect.territory,
+                    millimeters: req.body.insect.millimeters
+                });
+                
+                
+        };
+        
+        if (req.body.tree.id && req.body.insect.id && (await tree.hasInsect(insect))) {
+            next({
+                    status: "special error",
+                    message: `Could not create an association`,
+                    details: `Association already exists between ${tree.tree} and ${insect.name}`
+                }); 
+        } else { // all good, adding assosiation
+          
+            tree.addInsect(insect); // need only one
+            //insect.addTree(tree);
+
+            res.json({
+                status: 'success',
+                message: 'Successfully created association',
+                data: {
+                    tree: tree,
+                    insect: insect
+                }
+            })
+
+        }
+
+
+
+    } catch (err) {
+        next({
+            status: "error",
+            message: 'Could not update new tree',
+            details: err.errors ? err.errors.map(item => item.message).join(', ') : err.message
+        });
+    }
+})
+////////////
+
+////////////
+router.get(['/joined', '/joined*'], async (req, res, next) => {    
+    res.json({
+        message: "Use '/' without 'joined' word to access '/joined' roots" 
+    });
+});
 
 // Export class - DO NOT MODIFY
 module.exports = router;
